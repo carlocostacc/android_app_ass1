34a5bfcbc8ac0eca3116c5bd3d444ac11eaf93a3
carlocostacc <carlo.costa1@hotmail.com>
2023-05-24 23:24:12 -0400

    First commit

---
+++ app/src/main/java/com/example/assignement12/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.assignement12;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button button_settings;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button_settings = findViewById(R.id.button_settings);
+
+        button_settings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Send_to_settings();
+
+            }
+        });
+
+
+    }
+    private void Send_to_settings(){
+        Intent intent = new Intent(MainActivity.this, SettingActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+
+    }
+}
---
+++ app/src/main/java/com/example/assignement12/dataActivity.java
@@ -0,0 +1,16 @@
+package com.example.assignement12;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class dataActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_data);
+
+    }
+}
+
